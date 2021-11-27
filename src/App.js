75d2610b8d144ce2c72ab3b3bc5dde35abb4d338
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-27 23:01:53 +0900

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import logo from "./logo.gif";
+import Dictionary from "./Dictionary.js";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      Hello
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer ">
+          <p>
+            <small>
+              😎This project was coded by{" "}
+              <a
+                href="https://lola-portfolio.netlify.app/"
+                title="View source code"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Funmilola
+              </a>{" "}
+              and is{" "}
+              <a
+                href="https://github.com/LolaDeveloper/dictionary-project"
+                title="View source code"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open-sourced on GitHub
+              </a>
+            </small>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
