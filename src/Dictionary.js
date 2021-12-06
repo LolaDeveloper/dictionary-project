93dfba641c194aa8fbc8163ba7a787197ea1a91b
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-12-06 21:49:18 +0900

    Integrated images API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
-  function handleResponse(response) {
+  let [photos, setPhotos] = useState(null);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search(event) {
     event.preventDefault();
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    // documentation: https://www.pexels.com/api/documentation/?
+    let pexelsApiKey =
+      "563492ad6f917000010000011610dae867e84e9b9d74e3e9eeb3e9de";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -29,12 +41,13 @@ export default function Dictionary() {
             className="input-form"
             type="search"
             autoFocus={true}
-            placeholder="Search English"
+            placeholder="Search English word"
             onChange={handleKeywordChange}
           />
         </form>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
