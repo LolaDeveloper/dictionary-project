1cdc05f87e87873ec589a6924e2a37ef64887f0c
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-12-08 21:58:50 +0900

    Update App.js
    included animated particles background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from "./logo.gif";
 import Dictionary from "./Dictionary.js";
-
+import Particles from "react-tsparticles";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <Particles />
         <header className="App-header mb-0">
           <div className="row">
             <div className="col-4">
